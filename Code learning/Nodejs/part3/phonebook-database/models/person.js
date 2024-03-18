d9d1a3efa11f8b42721ad914525f9af7a1e77e9b
Hkchuen03 <hokingc1003@gmail.com>
2024-03-18 16:35:41 +0800

    update part 3

--- Code learning/Nodejs/part3/phonebook-database/models/person.js
+++ Code learning/Nodejs/part3/phonebook-database/models/person.js
@@ -7,7 +7,7 @@ const url =process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -15,8 +15,21 @@ mongoose.connect(url)
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      minlength: 8,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{4}/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    },
 })
 
 personSchema.set('toJSON', {
